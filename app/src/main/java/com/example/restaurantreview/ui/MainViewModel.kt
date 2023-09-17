501d4d69d580574cd1882ca1473c28b866f6f3ce
Nabila Khairunnisa <107665829+nabilaakhairunnisa@users.noreply.github.com>
2023-09-17 08:12:20 +0700

    Update MainViewModel.kt
    Single Event

--- app/src/main/java/com/example/restaurantreview/ui/MainViewModel.kt
+++ app/src/main/java/com/example/restaurantreview/ui/MainViewModel.kt
@@ -7,6 +7,7 @@ import com.example.restaurantreview.data.response.PostReviewResponse
 import com.example.restaurantreview.data.response.Restaurant
 import com.example.restaurantreview.data.response.RestaurantResponse
 import com.example.restaurantreview.data.retrofit.ApiConfig
+import com.example.restaurantreview.util.Event
 import retrofit2.Callback
 import retrofit2.Call
 import retrofit2.Response
@@ -21,6 +22,9 @@ class MainViewModel : ViewModel() {
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _snackbarText = MutableLiveData<Event<String>>()
+    val snackbarText: LiveData<Event<String>> = _snackbarText
 
     companion object{
         private const val TAG = "MainViewModel"
@@ -63,6 +67,7 @@ class MainViewModel : ViewModel() {
                 _isLoading.value = false
                 if (response.isSuccessful) {
                     _listReview.value = response.body()?.customerReviews
+                    _snackbarText.value = Event(response.body()?.message.toString())
                 } else {
                     Log.e(TAG, "onFailure: ${response.message()}")
                 }
